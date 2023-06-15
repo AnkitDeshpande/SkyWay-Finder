c727e283575cf349f14e4bf19be882d9d600e36b
Ankit Deshpande <urstrulyankit@protonmail.com>
2023-06-15 22:41:14 +0530

    admin_side_done

--- Project_FBS/src/main/java/com/fbs/Dao/CompanyDaoImpl.java
+++ Project_FBS/src/main/java/com/fbs/Dao/CompanyDaoImpl.java
@@ -1,6 +1,7 @@
 package com.fbs.Dao;
 
 import java.util.List;
+import java.util.Scanner;
 
 import com.fbs.Entity.Company;
 import com.fbs.Exception.NoRecordFoundException;
@@ -9,6 +10,7 @@ import com.fbs.Utility.EMUtils;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
 
 public class CompanyDaoImpl implements CompanyDAO {
 
@@ -16,45 +18,99 @@ public class CompanyDaoImpl implements CompanyDAO {
 	public void saveCompany(Company company) throws SomethingWentWrongException {
 		EntityManager em = null;
 		EntityTransaction et = null;
-		
+
 		try {
 			em = EMUtils.connect();
 			et = em.getTransaction();
-			
+
 			et.begin();
 			em.persist(company);
 			et.commit();
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-		}finally {
+		} finally {
 			em.close();
 		}
 
 	}
 
 	@Override
-	public void updateCompany(Company company) throws NoRecordFoundException, SomethingWentWrongException {
-		// TODO Auto-generated method stub
+	public void updateCompany(Company c) throws NoRecordFoundException, SomethingWentWrongException {
+		System.out.println("Enter the company ID: ");
+		Scanner sc = new Scanner(System.in);
+		Long companyId = sc.nextLong();
+		EntityManager em = null;
+		EntityTransaction et = null;
+		try {
+			em = EMUtils.connect();
+			et = em.getTransaction();
+
+			Company company = em.find(Company.class, companyId);
+
+			et.begin();
+			company.setName(c.getName());
+			company.setLocation(c.getLocation());
+			company.setContactNumber(c.getContactNumber());
+			company.setEmail(c.getEmail());
+			company.setWebsite(c.getWebsite());
+			et.commit();
+
+		} catch (Exception e) {
+			et.rollback();
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+		}
 
 	}
 
 	@Override
-	public void deleteCompany(Company company) throws NoRecordFoundException, SomethingWentWrongException {
-		// TODO Auto-generated method stub
+	public void deleteCompany(Company c) throws NoRecordFoundException, SomethingWentWrongException {
+		EntityManager em = null;
+		EntityTransaction et = null;
+		try {
+			em = EMUtils.connect();
+			et = em.getTransaction();
+
+			Company company = em.find(Company.class, c.getId());
+
+			et.begin();
+			em.remove(company);
+			et.commit();
+
+		} catch (Exception e) {
+			et.rollback();
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+		}
 
 	}
 
 	@Override
 	public Company getCompanyById(int companyId) throws NoRecordFoundException, SomethingWentWrongException {
-		// TODO Auto-generated method stub
-		return null;
+		try (EntityManager em = EMUtils.connect()) {
+			Company company = em.find(Company.class, (long) companyId);
+			if (company == null) {
+				throw new NoRecordFoundException("Company not found");
+			}
+			return company;
+		} catch (Exception e) {
+			throw new SomethingWentWrongException("Something went wrong while retrieving company");
+		}
 	}
 
 	@Override
 	public List<Company> getAllCompanies() throws SomethingWentWrongException {
-		// TODO Auto-generated method stub
-		return null;
+		try (EntityManager em = EMUtils.connect()) {
+			String Q = "Select c from Company c";
+			Query query = em.createQuery(Q);
+			List<Company> companies = query.getResultList();
+			return companies;
+		} catch (Exception e) {
+			throw new SomethingWentWrongException("Something went wrong while retrieving company");
+		}
 	}
 
 }
--- Project_FBS/src/main/java/com/fbs/UI/MainUI.java
+++ Project_FBS/src/main/java/com/fbs/UI/MainUI.java
@@ -28,57 +28,70 @@ public class MainUI {
 	}
 	
 	static void adminOperations() {
-		System.out.println("1. Add a company");
-		System.out.println("2. Update Company.");
-		System.out.println("3. Delete Company");
-		System.out.println("4. View All company ");
-		System.out.println("5. Add a Flight");
-		System.out.println("6. Update a Flight ");
-		System.out.println("7. Delete a Flight ");
-		System.out.println("8. View All Flights");
-		System.out.println("9. Get all Users ");
-		System.out.println("0. Logout");
+	    System.out.println("1. Add a company");
+	    System.out.println("2. Update Company.");
+	    System.out.println("3. Delete Company");
+	    System.out.println("4. View Company by ID");
+	    System.out.println("5. View All companies");
+	    System.out.println("6. Add a Flight");
+	    System.out.println("7. Update a Flight");
+	    System.out.println("8. Delete a Flight");
+	    System.out.println("9. get a Flight by Id");
+	    System.out.println("10. View All Flights");
+	    System.out.println("11. Get all Users");
+	    System.out.println("0. Logout");
 	}
-	
+
 	static void adminMenu(Scanner sc) {
-		int choice = 0;
-		do {
-			adminOperations();
-			System.out.print("Enter selection ");
-			choice = sc.nextInt();
-    		switch(choice) {
-    			case 1:
-    				AdminUI.addCompany(sc);
-    				break;
-    			case 2:
-    				AdminUI.updateCompany(sc);
-    				break;
-    			case 3:
-    				AdminUI.deletecompany(sc);
-    				break;
-    			case 4:
-    				AdminUI.viewAllCompanies(sc);
-    				break;
-    			case 5:
-    				AdminUI.addFlight();
-    	            break;
-    	        case 6:
-    	        	AdminUI.updateFlight();
-    	            break;
-    	        case 7:
-    	        	AdminUI.deleteFlight();
-    	            break;
-    	        case 8:
-    	        	AdminUI.viewAllFlights();
-    	            break;
-    	        case 9:
-    	        	AdminUI.getAllUsers();
-    	            break;
-    			case 0:
-    				System.out.println("Bye Bye Admin");
-    				break;
-    			default:
-    				System.out.println("Invalid Selection, try again");
+	    int choice = 0;
+	    do {
+	        adminOperations();
+	        System.out.print("Enter selection: ");
+	        choice = sc.nextInt();
+	        switch (choice) {
+	            case 1:
+	                AdminUI.addCompany(sc);
+	                break;
+	            case 2:
+	                AdminUI.updateCompany(sc);
+	                break;
+	            case 3:
+	                AdminUI.deletecompany(sc);
+	                break;
+	            case 4:
+	                AdminUI.viewCompanyById(sc);
+	                break;
+	            case 5:
+	                AdminUI.viewAllCompanies();
+	                break;
+	            case 6:
+	                AdminUI.addFlight(sc);
+	                break;
+	            case 7:
+	                AdminUI.updateFlight(sc);
+	                break;
+	            case 8:
+	                AdminUI.deleteFlight(sc);
+	                break;
+	            case 9:AdminUI.getFlightById(sc);
+	            	break;
+	            case 10:
+	                AdminUI.viewAllFlights();
+	                break;
+	            case 11:
+	                AdminUI.getAllUsers();
+	                break;
+	            case 12:
+	            	AdminUI.getFlightsByCompany(sc);
+	            	break;
+	            case 13:
+	            	AdminUI.getFlightsByPassenger(sc);
+	            	break;
+	            case 0:
+	                System.out.println("Bye Bye Admin");
+	                break;
+	            default:
+	                System.out.println("Invalid Selection, try again");
     		}
     	}while(choice != 0);	
 	}
